af5868433f0311d6de73859bacf62c1317e5f036
GuowenGao <funte@outlook.com>
2020-03-23 11:57:23 +0800

    test: 测试 fs 和 str

---
+++ test/fs.test.js
@@ -0,0 +1,30 @@
+const pig = require('../src/index.js');
+var assert = require('assert');
+var path = require('path');
+
+describe('fs', function () {
+  describe('#walk()', function () {
+    it('同步历目录', function (done) {
+      var files = ['a', 'b'];
+      pig.fs.walk(path.resolve('test/.fs.test'), file => {
+        var index = files.indexOf(path.basename(file));
+        assert.notEqual(index, -1);
+        files.splice(index, 1);
+        if (files.length == 0) {
+          done();
+        }
+      });
+    });
+  });
+  describe('#walkSync()', function () {
+    it('异步遍历目录', function () {
+      var files = ['a', 'b'];
+      pig.fs.walkSync(path.resolve('test/.fs.test'), file => {
+        var index = files.indexOf(path.basename(file));
+        assert.notEqual(index, -1);
+        files.splice(index, 1);
+      });
+      assert.equal(files.length, 0);
+    });
+  });
+});
---
+++ test/str.test.js
@@ -0,0 +1,13 @@
+const pig = require('../src/index.js');
+const assert = require('assert');
+
+describe('str', function () {
+  describe('#unixlike()', function () {
+    it('将路径字符串中的 `\\` 转换为 `/`', function () {
+      assert.equal(
+        pig.str.unixlike('..\\..\\src\\index.js'),
+        '../../src/index.js'
+      );
+    });
+  });
+});
