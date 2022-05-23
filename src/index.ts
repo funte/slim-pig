dda9ab2de642d6ce8213863f434b323273fb1c55
Guowen Gao <funte@outlook.com>
2022-05-23 08:38:17 +0800

    chore: update pcakage export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-export * as fs from './lib/fs';
-export * as func from './lib/func';
-export * as str from './lib/str';
-export * as pattern from './lib/pattern';
+import * as fs from './lib/fs';
+import * as func from './lib/func';
+import * as str from './lib/str';
+import * as pattern from './lib/pattern';
+
+export {
+  fs,
+  func,
+  str,
+  pattern
+}
